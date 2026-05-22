c61e8e601ed5fa0eaec72766cde8b8a03fc73c68
Nadya Aulia Salma <nadyaauliasalma622@gmail.com>
2026-05-22 19:16:47 +0700

    Clean up Driver.java by removing unused code
    Removed unused variables and dead code, and replaced repeated increments with a loop.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,27 +5,16 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
-		Counter counter2 = new Counter(); // unused variable
-		int x = 0; // unused variable
 		
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.increment();
-		counter.increment();
-		counter.increment();
-		counter.increment();
-		counter.increment(); // duplikat berulang, tidak pakai loop
 		
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount());
-		
-		// dead code
-		if (false) {
-			System.out.println("ini tidak akan pernah dieksekusi");
-		}
 		
 	}
 
